15c4f6a45f884a088497995b7a1c83ecb47960e0
soham-parab <parabsoham@gmail.com>
2020-12-19 13:43:17 +0530

    added output

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("script is workig fron asdmasoa")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output");
+
+
+outputDiv.innerText = "Soham Parab"
+console.log(outputDiv
+    )
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+
+};
+
+btnTranslate.addEventListener("click", clickHandler)
